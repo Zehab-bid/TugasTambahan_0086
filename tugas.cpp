b05775f01017674b043cd7268dcf014b41367c97
Zehab-bid <zehabidha152gmail.com>
2025-05-01 14:40:22 +0700

    proges 4

--- tugas.cpp
+++ tugas.cpp
@@ -15,4 +15,13 @@ class Segitiga {
             cout << "Masukkan Tinggi Segitiga: ";
             cin >> tinggi;
         }
-    }
+
+        void outputData() {
+            if (alas % 2 == 1) { // Jika alas ganjil
+                cout << "Alas yang dimasukkan adalah bilangan ganjil, luas tidak dapat dihitung." << endl;
+            } else { 
+                float luasSegitiga = luas(alas, tinggi);
+                cout << "Luas Segitiga: " << luasSegitiga << endl;
+            }
+        }
+    };
